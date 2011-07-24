3e6d66c5fe1176a60cd6f2055ddad1243dacd966
utahta <labs.ninxit@gmail.com>
2011-07-24 16:01:14 +0900

    hello world

--- initdb.py
+++ initdb.py
@@ -4,8 +4,8 @@ from app.db import db_engine, db_session
 
 try:
     ProjectTable.__table__.create(bind=db_engine)
-    db_session.add(ProjectTable('Beer'))
-    db_session.add(ProjectTable(u'ビール'))
+    db_session.add(ProjectTable('Hello World!'))
+    db_session.add(ProjectTable(u'こんにちは、世界！'))
     db_session.commit()
 except:
     pass
